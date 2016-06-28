9b3796965a3f2bf1e30e9d40df2e3c6c5f8f567a
Jea <yiyj@yunjiacloud.com>
2016-06-28 19:03:43 +0800

    add

--- src/main/java/com/yiyj/register/controller/RegisterController.java
+++ src/main/java/com/yiyj/register/controller/RegisterController.java
@@ -1,23 +1,35 @@
 package com.yiyj.register.controller;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import voice.voice_register_client.service.RegisterSVC;
+
 @Controller
 public class RegisterController {
-	
-	@RequestMapping(value="/register.do")
+
+	@Resource
+	RegisterSVC registerSVC;
+
+	@RequestMapping(value = "/register.do")
 	public ModelAndView register(HttpServletRequest request) {
 		ModelAndView mv = new ModelAndView("register/register");
 		return mv;
 	}
-	
-	@RequestMapping(value="/toRegister.do")
-	public void register(HttpServletRequest request,String userName,String password) {
-		
+
+	@RequestMapping(value = "/toRegister.do")
+	public void register(HttpServletRequest request, String userName, String password) {
+		if (userName.isEmpty() || password.isEmpty()) {
+			
+		}
+		boolean register = registerSVC.register(userName, password);
+		if (!register) {
+			
+		}
 	}
-	
+
 }
